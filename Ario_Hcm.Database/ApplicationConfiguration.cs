2f27d9e84ba927b6c6ee605c980d9bc8fd467333
arioanindito <arioanindito@hotmail.com>
2021-12-29 08:10:58 +0200

    initial

---
+++ Ario_Hcm.Database/ApplicationConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ario_Hcm.Database
+{
+    public static class ApplicationConfiguration
+    {
+        public static IServiceCollection AddDatabase(this IServiceCollection services)
+        {
+            services.AddDbContext<DatabaseContext>(configure =>
+           {
+               configure.UseSqlite("Filename=main.db");
+               configure.EnableDetailedErrors(true);
+               configure.ConfigureWarnings(opt =>
+               {
+                   opt.Default(WarningBehavior.Log);
+               });
+
+           });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseDatabase(this IApplicationBuilder app)
+        {
+            var context = app.ApplicationServices.CreateScope()
+                .ServiceProvider
+                .GetRequiredService<DatabaseContext>();
+
+            context.Database.Migrate();
+
+            return app;
+        }
+    }
+}
---
+++ Ario_Hcm.Database/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ario_Hcm.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
